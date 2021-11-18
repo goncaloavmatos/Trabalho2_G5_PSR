c25bac4bd527d82e1d5d7fb4c0bdd8b63ec7a505
carlosmprocha <carlosmpr@ua.pt>
2021-11-18 01:13:09 +0000

    Esqueleto inicial

--- ar_paint.py
+++ ar_paint.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import argparse
 import json
-import copy
 import cv2
 import numpy as np
 
@@ -15,33 +14,32 @@ def main():
     parser.add_argument('-j', '--json', type=str, required=True, help='Full path to json file.')
     args = vars(parser.parse_args())
 
+    # Get limits.json
+    with open("limits.json", "r") as f:
+        ranges = json.load(f)
 
-    global image
-    capture = cv2.VideoCapture(0)
-    window_name = 'Capture'
-    cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
-
-
-    print(image)
-    white_scr = np.zeros([512, 512, 1], dtype=np.uint8)
+    white_scr = np.zeros([600, 600, 1], dtype=np.uint8)
     white_scr.fill(255)
 
     cv2.imshow('White Screen', white_scr)
     print("image shape: ", white_scr.shape)
 
-    # Get limits.json
-    with open("limits.json", "r") as f:
-        ranges = json.load(f)
-    # print(ranges)
+    cam = cv2.VideoCapture(0)
 
-
+    print('Press q to exit')
 
     while True:
-        _, image = capture.read()  # get an image from the camera
+        ret, frame = cam.read()  # get an image from the camera
 
-        cv2.imshow(window_name, image)
+        cv2.namedWindow('Capture', cv2.WINDOW_AUTOSIZE)
+        resized = cv2.resize(frame, (600, 600), interpolation=cv2.INTER_AREA)
+        print("capture size: ", resized.shape)
+        cv2.imshow('Capture', resized)
 
-        cv2.waitKey(0)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cam.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
